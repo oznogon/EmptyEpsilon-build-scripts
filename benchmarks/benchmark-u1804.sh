024ae531e5ea6426a64ddf717fbb484615f33f5d
Oznogon <garrett+ozgh@oznogon.com>
2020-03-17 19:50:32 -0700

    Add benchmarks

---
+++ benchmarks/benchmark-u1804.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+for i in 1 3 6 12
+do
+  for j in win32 linux android
+  do
+    rm -rf EmptyEpsilon/_build_${j}
+    echo "Testing ${j} with ${i} threads..."
+    facter > time-${i}-${j}.bench
+    (
+      cd ..
+      time ./build_ee_u1804.sh noupdate threads${i} ${j}
+    ) >> time-${i}-${j}.bench 2>&1
+    echo "Testing ${j} with ${i} threads complete."
+    tail -n 5 time-${i}-${j}.bench
+  done
+done
